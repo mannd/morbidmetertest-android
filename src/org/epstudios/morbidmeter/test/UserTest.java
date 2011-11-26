68efc77d20a96073908d79d5444bfb6c6f606c72
David Mann <mannd@epstudiossoftware.com>
2011-11-26 11:29:28 -0500

    first commit

---
+++ src/org/epstudios/morbidmeter/test/UserTest.java
@@ -0,0 +1,46 @@
+package org.epstudios.morbidmeter.test;
+
+import java.util.Calendar;
+
+import org.epstudios.morbidmeter.User;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+	public UserTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testUser() {
+		User user = new User("default", Calendar.getInstance(), 80.0);
+	}
+
+	public void testDeathDay() {
+		Calendar bd = Calendar.getInstance();
+		bd.set(Calendar.YEAR, 1950);
+		bd.set(Calendar.MONTH, 1);
+		bd.set(Calendar.DAY_OF_MONTH, 1);
+		User user = new User("default", bd, 80.0);
+		int deathYear = user.deathDay().get(Calendar.YEAR);
+		assertEquals(2030, deathYear);
+	}
+
+	public void testPercentAlive() {
+		Calendar bd = Calendar.getInstance();
+		bd.set(Calendar.YEAR, 1950);
+		bd.set(Calendar.MONTH, 1);
+		bd.set(Calendar.DAY_OF_MONTH, 1);
+		User user = new User("default", bd, 80.0);	
+		Calendar testDate = Calendar.getInstance();
+		testDate.set(Calendar.YEAR, 1990);
+		testDate.set(Calendar.MONTH, 1);
+		testDate.set(Calendar.DAY_OF_MONTH, 1);
+		assertEquals(0.5, user.percentAlive(testDate), 0.01);
+	}
+
+}
