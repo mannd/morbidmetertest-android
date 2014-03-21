d2f85755a1f0581cd60594049a2fc0116169d2d9
David Mann <mannd@epstudiossoftware.com>
2014-03-21 21:18:04 +0100

    fixed tests

--- src/org/epstudios/morbidmeter/test/NotificationTest.java
+++ src/org/epstudios/morbidmeter/test/NotificationTest.java
@@ -2,7 +2,7 @@ package org.epstudios.morbidmeter.test;
 
 import junit.framework.TestCase;
 
-import org.epstudios.morbidmeter.MorbidMeter;
+import org.epstudios.morbidmeter.MorbidMeterClock;
 
 public class NotificationTest extends TestCase {
 
@@ -11,20 +11,22 @@ public class NotificationTest extends TestCase {
 	}
 
 	public void testIsMilestone() {
-		assert (MorbidMeter.isEvenHour("Jan 4, 2012 5:00:03 AM 414 msec"));
-		assert (!MorbidMeter.isEvenHour("Jan 4, 2012 5:01:03 AM 414 msec"));
-		assert (!MorbidMeter.isEvenHour("Jan 4, 2012 5:01:00 AM 414 msec"));
-		assert (MorbidMeter.isEvenMinute("5:00:00 PM"));
-		assert (!MorbidMeter.isEvenMinute("4:01:01"));
-		assert (MorbidMeter.isEvenPercentage("99.01111"));
-		assert (!MorbidMeter.isEvenPercentage("99.100000"));
-		assert (MorbidMeter.isEvenMillion("5,000,011 years"));
-		assert (MorbidMeter.isEvenMillion("12,000,101,000,111 years"));
-		assert (!MorbidMeter.isEvenMillion("14,001,001,000 years"));
+		assert (MorbidMeterClock.isEvenHour("Jan 4, 2012 5:00:03 AM 414 msec"));
+		assert (!MorbidMeterClock.isEvenHour("Jan 4, 2012 5:01:03 AM 414 msec"));
+		assert (!MorbidMeterClock.isEvenHour("Jan 4, 2012 5:01:00 AM 414 msec"));
+		assert (MorbidMeterClock.isEvenMinute("5:00:00 PM"));
+		assert (!MorbidMeterClock.isEvenMinute("4:01:01"));
+		assert (MorbidMeterClock.isEvenPercentage("99.01111"));
+		assert (!MorbidMeterClock.isEvenPercentage("99.100000"));
+		assert (MorbidMeterClock.isEvenMillion("5,000,011 years"));
+		assert (MorbidMeterClock.isEvenMillion("12,000,101,000,111 years"));
+		assert (!MorbidMeterClock.isEvenMillion("14,001,001,000 years"));
 		// test with carriage returns
-		assert (MorbidMeter.isEvenHour("Jan 4\n 2012\n 1:00:00 PM 333 msec"));
+		assert (MorbidMeterClock
+				.isEvenHour("Jan 4\n 2012\n 1:00:00 PM 333 msec"));
 	}
 
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
